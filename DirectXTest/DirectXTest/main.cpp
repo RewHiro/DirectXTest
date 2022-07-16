b688ce027f8bb600c955a1eece955c8cc1bc2def
RewHiro <hiro1215.turquoise@gmail.com>
2022-07-16 14:55:21 +0900

    ・四角形で表示 ・インデックスで描画

--- DirectXTest/DirectXTest/main.cpp
+++ DirectXTest/DirectXTest/main.cpp
@@ -255,9 +255,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSRT, int)
 
 	DirectX::XMFLOAT3 vertices[] =
 	{
-		{-0.5f,-0.7f,0.0f}, //左下
-		{0.0f,0.7f,0.0f}, //左上
-		{0.5f,-0.7f,0.0f}, //右下
+		{-0.4f,-0.7f,0.0f}, //左下
+		{-0.4f,0.7f,0.0f}, //左上
+		{0.4f,-0.7f,0.0f}, //右下
+		{0.4f,0.7f,0.0f}, //右上
+	};
+
+	unsigned short indices[] =
+	{
+		0, 1, 2,
+		2, 1, 3
 	};
 
 	D3D12_HEAP_PROPERTIES heapprop = {};
@@ -297,6 +304,34 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSRT, int)
 	vbView.BufferLocation = vertBuff->GetGPUVirtualAddress(); // バッファーの仮想アドレス
 	vbView.SizeInBytes = sizeof(vertices); // 全バイト数
 	vbView.StrideInBytes = sizeof(vertices[0]); // 1頂点あたりのバイト数
+
+	ID3D12Resource* idxBuff = nullptr;
+
+	// 設定は、バッファーのサイズ以外、頂点バッファの設定を使いまわしてよい
+	resdesc.Width = sizeof(indices);
+
+	result = _dev->CreateCommittedResource
+	(
+		&heapprop,
+		D3D12_HEAP_FLAG_NONE,
+		&resdesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&idxBuff)
+	);
+
+	// 作ったバッファーにインデックスデータをコピー
+	unsigned short* mappedIdx = nullptr;
+	result = idxBuff->Map(0, nullptr, (void**)&mappedIdx);
+	std::copy(std::begin(indices), std::end(indices), mappedIdx);
+	idxBuff->Unmap(0, nullptr);
+
+	// インデックスバッファビューを作成
+	D3D12_INDEX_BUFFER_VIEW ibView = {};
+	ibView.BufferLocation = idxBuff->GetGPUVirtualAddress(); // バッファーの仮想アドレス
+	ibView.Format = DXGI_FORMAT_R16_UINT;
+	ibView.SizeInBytes = sizeof(indices); // 全バイト数
+
 
 	ID3DBlob* _vsBlob = nullptr;
 	ID3DBlob* _psBlob = nullptr;
@@ -467,7 +502,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSRT, int)
 	_cmdList->RSSetScissorRects(1, &scissorrect);
 	_cmdList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 	_cmdList->IASetVertexBuffers(0, 1, &vbView);
-	_cmdList->DrawInstanced(3, 1, 0, 0 );
+	_cmdList->IASetIndexBuffer(&ibView);
+	_cmdList->DrawIndexedInstanced(6, 1, 0, 0, 0);
 
 	BarrierDesc.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
 	BarrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
